aa16df81145194099a520699835cafbc017eb53c
HilalBudak <hilalbudak.97@gmail.com>
2019-07-09 20:46:41 +0300

    geliştirmeler yapıldı

--- GameOfLife.c
+++ GameOfLife.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
+#define satir 5
+#define sutun 20
+
+
+int matris[satir][sutun]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int print()
+{
+    int i,j;
+    for(i=0;i<satir;++i)
+    {
+        printf("\n");
+        for(j=0;j<sutun;++j)
+        {
+            if(matris[i][j]==1)
+                printf("*");
+            else
+                printf(" ");
+                 
+        }
+    }
+}
+
 
 int main()
-{
-    int matris[3][5]={1,0,1,0,0,
-                      0,1,0,0,1,
-                      1,1,1,0,1 };
+{ 
+    //print();                          print fonksiyonu test edildi.
     int i,j,sayac=0,k=0;
-    while (k<5)
+    while (k<5)                         //kodun çalışmasını rahat gözlemlemek için while 5 ile sınırlandırıldı.
     {       
-        for(i=0;i<3;++i)
-        {
-            printf("\n");
-            for(j=0;j<5;++j)
+        for(i=0;i<satir;++i)
+        {           
+            for(j=0;j<sutun;++j)
             {
                 sayac=0;
 
@@ -34,17 +58,21 @@ int main()
                 {
                     if(sayac<2 || sayac>3)
                         matris[i][j]=0;
-                    if(sayac==2 || sayac==3)
-                        matris[i][j]=1;    
+                                                
+                    if(sayac==2 || sayac==3)               
+                        matris[i][j]=1;
+                            
                 }
                 else
                 {
                     if(sayac==3)
                         matris[i][j]==1;
+                        
                 }               
-                printf("%d",matris[i][j]);              
+                print();             
             }
-        }    
+        
+        }           
         k++;
         printf("\n");              
     }
