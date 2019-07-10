2154d15bc081b632638340c739d3380af8781aec
HilalBudak <hilalbudak.97@gmail.com>
2019-07-10 13:45:04 +0300

    değişiklikler uygulandı

--- GameOfLife.c
+++ GameOfLife.c
@@ -3,9 +3,9 @@
 #define sutun 20
 
 
-int matris[satir][sutun]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-                            0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-                            0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+int matris[satir][sutun]= {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,
+                            0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,
+                            0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,
                             0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
@@ -20,62 +20,81 @@ int print()
             if(matris[i][j]==1)
                 printf("*");
             else
-                printf(" ");
+                printf(".");
                  
         }
     }
+}
+
+int game()
+{
+    int i,j,sayac=0;
+    print();
+    for(i=0;i<satir;i++)
+    {           
+        for(j=0;j<sutun;j++)
+        {
+            sayac=0;
+
+            if(matris[i][j+1]==1)
+                sayac++;
+            if(matris[i+1][j]==1)
+                sayac++;
+            if(matris[i+1][j+1]==1)
+                sayac++;
+            if(i>0 && j>0 && matris[i-1][j-1]==1)
+                sayac++;
+            if(i>0 && matris[i-1][j]==1)
+                sayac++;
+            if(i>0 && matris[i-1][j+1]==1)
+                sayac++;
+            if(j>0 && matris[i+1][j-1]==1)
+                sayac++;    
+            if(j>0 && matris[i][j-1]==1)
+                sayac++;                
+
+            if(matris[i][j]==1)
+            {
+                if(sayac<2 || sayac>3)
+                {
+                    matris[i][j]=0;
+                    print();
+                }
+                    
+                                                
+                if(sayac==2 || sayac==3)  
+                {
+                    matris[i][j]=1;
+                    print();
+                }             
+                                                
+            }
+            else
+            {
+                if(sayac==3)
+                {
+                    matris[i][j]==1;
+                    print();
+                }
+                                         
+            } 
+                                              
+        }
+        
+    }
+              
 }
 
 
 int main()
 { 
     //print();                          print fonksiyonu test edildi.
-    int i,j,sayac=0,k=0;
-    while (k<5)                         //kodun çalışmasını rahat gözlemlemek için while 5 ile sınırlandırıldı.
+    int k=0;
+    while (k<10)                         //kodun çalışmasını rahat gözlemlemek için while 5 ile sınırlandırıldı.
     {       
-        for(i=0;i<satir;++i)
-        {           
-            for(j=0;j<sutun;++j)
-            {
-                sayac=0;
-
-                if(matris[i][j+1]==1)
-                    sayac++;
-                if(matris[i+1][j]==1)
-                    sayac++;
-                if(matris[i+1][j+1]==1)
-                    sayac++;
-                if(i>0 && j>0 && matris[i-1][j-1]==1)
-                    sayac++;
-                if(i>0 && matris[i-1][j]==1)
-                    sayac++;
-                if(i>0 && matris[i-1][j+1]==1)
-                    sayac++;
-                if(j>0 && matris[i+1][j-1]==1)
-                    sayac++;                
-
-                if(matris[i][j]==1)
-                {
-                    if(sayac<2 || sayac>3)
-                        matris[i][j]=0;
-                                                
-                    if(sayac==2 || sayac==3)               
-                        matris[i][j]=1;
-                            
-                }
-                else
-                {
-                    if(sayac==3)
-                        matris[i][j]==1;
-                        
-                }               
-                print();             
-            }
-        
-        }           
+        game();
         k++;
-        printf("\n");              
+        //printf("\n");              
     }
-    
-    
+     
 }
